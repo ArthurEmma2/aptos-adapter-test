10358c057603e9daca7d78fab9080585651ee776
Arthur Emmanuel <emmanuel2002arthur@gmail.com>
2024-01-03 20:30:01 +0000

    First-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
 
 function App() {
+  const { wallets, wallet, connect, disconnect, network, connected, account } =
+    useWallet();
+
+  const handleConnectClick = async (walletName: any) => {
+    if (connected) {
+      // If already connected, disconnect on click
+      await disconnect();
+    } else {
+      // If not connected, connect on click
+      await connect(walletName);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>APTOS ADAPTER TEST</p>
+
+        {wallets.map((val) => (
+          <button
+            key={val.name}
+            onClick={() => handleConnectClick(val.name)}
+            style={{
+              borderRadius: "8px",
+              height: "2.5rem",
+              width: "10rem",
+              fontSize: "1rem",
+            }}
+          >
+            <div className="flex flex-row justify-between items-center gap-x-9">
+              <img
+                alt="icon"
+                src={val.icon}
+                width={30}
+                height={30}
+                className="block rounded-full"
+              />
+              {val.name}
+            </div>
+          </button>
+        ))}
+
+        {connected && (
+          <p style={{ marginTop: "20px", fontSize: "1.2rem" }}>
+            Wallet Connected
+          </p>
+        )}
       </header>
     </div>
   );
